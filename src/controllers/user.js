ef9a86078702612391bce4a43894bc1633f7b3a2
tvaltn <valtniemi@gmail.com>
2024-10-28 15:49:25 +0100

    test commit

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,6 +1,6 @@
 import User from '../domain/user.js'
 import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
-
+// Test commit
 export const create = async (req, res) => {
   const userToCreate = await User.fromJson(req.body)
 
