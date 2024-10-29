04b4a9af77fabf37a3d0bcebb0254a3365bad635
Ingeborg Brommeland Austeid <ingeborg_austeid@hotmail.com>
2024-10-29 11:46:49 +0100

    adds  in routes\user.js instead of in controllers\user.js

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,27 +1,23 @@
 import User from '../domain/user.js'
-import { validateUser } from '../middleware/user.js'
 import { sendDataResponse, sendMessageResponse } from '../utils/responses.js'
 
-export const create = [
-  validateUser,
-  async (req, res) => {
-    const userToCreate = await User.fromJson(req.body)
+export const create = async (req, res) => {
+  const userToCreate = await User.fromJson(req.body)
 
-    try {
-      const existingUser = await User.findByEmail(userToCreate.email)
+  try {
+    const existingUser = await User.findByEmail(userToCreate.email)
 
-      if (existingUser) {
-        return sendDataResponse(res, 400, { email: 'Email already in use' })
-      }
-
-      const createdUser = await userToCreate.save()
-
-      return sendDataResponse(res, 201, createdUser)
-    } catch (error) {
-      return sendMessageResponse(res, 500, 'Unable to create new user')
+    if (existingUser) {
+      return sendDataResponse(res, 400, { email: 'Email already in use' })
     }
+
+    const createdUser = await userToCreate.save()
+
+    return sendDataResponse(res, 201, createdUser)
+  } catch (error) {
+    return sendMessageResponse(res, 500, 'Unable to create new user')
   }
-]
+}
 
 export const getById = async (req, res) => {
   const id = parseInt(req.params.id)
